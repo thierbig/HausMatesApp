ea54e49933a3c54528734897eb5b21597a98cbb6
My Name <gamerproject@protonmail.com>
2023-08-20 23:42:46 -0400

    first commit

---
+++ app/src/main/java/com/example/jr/hausmatesapp/Application.java
@@ -0,0 +1,17 @@
+package com.example.jr.hausmatesapp;
+
+import android.support.v4.util.Pools;
+
+/**
+ * Created by thomas on 21/09/16.
+ */
+public class Application
+{
+    static Pool[] localTasks = new Pool[];
+    static User[] localContacts = new User[];
+    static Notification[] localNotifications = new Notification[];
+    static User localUser = new User;
+
+
+
+}
